1ddb5c688bfd4283ff9703b95f0d358e34c3d091
Masadekaiketu <masa.kikai0304@gmail.com>
2022-05-26 16:23:35 +0900

    add connected component

--- full_dfs.py
+++ full_dfs.py
@@ -5,11 +5,18 @@ def full_dfs(Adj):
     for v in range(len(Adj)):
         if parent[v] is None:
             parent[v] = v
-            dfs(Adj, v, parent, order) #each connected component is stored in order as list
+            order_sub = []
+            dfs(Adj, v, parent, order_sub) #each connected component is stored in order as list
+            order.append(order_sub) #len(order) is the number of connected component
     return parent, order
 
-Adj =  [[1, 2],
+Adj =  [[1],
         [2],
         [3],
-        [3]]
+        [4],
+        [5],
+        [],
+        [7],
+        [8],
+        []]
 print(full_dfs(Adj))
